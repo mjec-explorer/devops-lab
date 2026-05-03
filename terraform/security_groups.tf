a42f1d27b408bbe3dfb88df58db512395118a496
MJ Castro <mjarcino19@gmail.com>
2026-05-03 23:58:46 +0200

    feat: fix security groups, add SSM, move Jenkins to private subnet

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -12,14 +12,6 @@ resource "aws_security_group" "alb" {
   }
 
   ingress {
-    description     = "FastAPI from ALB only"
-    from_port       = 8000
-    to_port         = 8000
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb.id]
-  }
-
-  ingress {
     description = "HTTPS from internet"
     from_port   = 443
     to_port     = 443
@@ -63,6 +55,14 @@ resource "aws_security_group" "ecs_tasks" {
     cidr_blocks = [var.private_subnet_1_cidr, var.private_subnet_2_cidr]
   }
 
+  ingress {
+    description     = "FastAPI from ALB only"
+    from_port       = 8000
+    to_port         = 8000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+
   egress {
     description = "ECR image pulls and CloudWatch logs"
     from_port   = 443
@@ -102,31 +102,6 @@ resource "aws_security_group" "monitoring" {
   name        = "${var.project_name}-monitoring-sg"
   description = "Security group for Monitoring"
   vpc_id      = aws_vpc.main.id
-
-
-  ingress {
-    description = "Grafana via Jenkins tunnel"
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = [aws_security_group.jenkins.id]
-  }
-
-  ingress {
-    description = "Prometheus via Jenkins tunnel"
-    from_port   = 9090
-    to_port     = 9090
-    protocol    = "tcp"
-    cidr_blocks = [aws_security_group.jenkins.id]
-  }
-
-  ingress {
-    description = "n8n via Jenkins tunnel"
-    from_port   = 5678
-    to_port     = 5678
-    protocol    = "tcp"
-    cidr_blocks = [aws_security_group.jenkins.id]
-  }
 
   egress {
     description     = "scrape node_exporter"
